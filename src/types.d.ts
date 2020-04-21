1c10e2e0e419ece0a069dee4751a2d12302edc06
demonitaste <35500074+demonitaste@users.noreply.github.com>
2020-04-21 13:59:59 +0200

    Initial commit

---
+++ src/types.d.ts
@@ -0,0 +1,27 @@
+// example declaration file - remove these and add your own custom typings
+
+// Shim to allow continued use of global lodash without having to explicitley import in every file.
+// as of @types/lodash@3.10.3
+import _ from "lodash";
+declare global {
+  const _: typeof _;
+}
+
+// memory extension samples
+interface CreepMemory {
+  role: string;
+  room: string;
+  working: boolean;
+}
+
+interface Memory {
+  uuid: number;
+  log: any;
+}
+
+// `global` extension samples
+declare namespace NodeJS {
+  interface Global {
+    log: any;
+  }
+}
