8ca48c90a10c5064260a978666891fa7b7d7f6a3
demonitaste <nan.nystrom@gmail.com>
2020-11-25 12:54:49 +0100

    changed rampart build order

--- src/config/base/baseCenter.ts
+++ src/config/base/baseCenter.ts
@@ -21,33 +21,30 @@ export const baseCenterLayout: BuildInstruction[][] = [
     ],
     [
         { x: 0, y: -2, type: STRUCTURE_STORAGE },
-        { x: 0, y: -2, type: STRUCTURE_RAMPART }
+        { x: 0, y: -2, type: STRUCTURE_RAMPART },
+        { x: -1, y: -1, type: STRUCTURE_RAMPART },
+        { x: -1, y: 1, type: STRUCTURE_RAMPART },
+        { x: 1, y: 1, type: STRUCTURE_RAMPART },
+        { x: 0, y: 2, type: STRUCTURE_RAMPART },
+        { x: 2, y: 0, type: STRUCTURE_RAMPART },
+        { x: 1, y: -1, type: STRUCTURE_RAMPART },
+        { x: -2, y: 0, type: STRUCTURE_RAMPART },
+        { x: 0, y: -1, type: STRUCTURE_RAMPART },
+        { x: 1, y: 0, type: STRUCTURE_RAMPART }
     ],
     [
         { x: 1, y: 0, type: STRUCTURE_TOWER },
-        { x: 1, y: 0, type: STRUCTURE_RAMPART },
-        { x: 0, y: -1, type: STRUCTURE_LINK },
-        { x: 0, y: -1, type: STRUCTURE_RAMPART }
+        { x: 0, y: -1, type: STRUCTURE_LINK }
     ],
-    [
-        { x: 0, y: 2, type: STRUCTURE_TERMINAL },
-        { x: 0, y: 2, type: STRUCTURE_RAMPART }
-    ],
+    [{ x: 0, y: 2, type: STRUCTURE_TERMINAL }],
     [
         { x: 1, y: -1, type: STRUCTURE_TOWER },
-        { x: 1, y: -1, type: STRUCTURE_RAMPART },
-        { x: 2, y: 0, type: STRUCTURE_FACTORY },
-        { x: 2, y: 0, type: STRUCTURE_RAMPART }
+        { x: 2, y: 0, type: STRUCTURE_FACTORY }
     ],
     [
         { x: -1, y: -1, type: STRUCTURE_TOWER },
         { x: -1, y: 1, type: STRUCTURE_TOWER },
         { x: 1, y: 1, type: STRUCTURE_TOWER },
-        { x: -1, y: -1, type: STRUCTURE_RAMPART },
-        { x: -1, y: 1, type: STRUCTURE_RAMPART },
-        { x: 1, y: 1, type: STRUCTURE_RAMPART },
-
-        { x: -2, y: 0, type: STRUCTURE_POWER_SPAWN },
-        { x: -2, y: 0, type: STRUCTURE_RAMPART }
+        { x: -2, y: 0, type: STRUCTURE_POWER_SPAWN }
     ]
 ];
