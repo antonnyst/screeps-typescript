06e69fe06fe07f490ebf22fda14bff0f96c3f164
demonitaste <nan.nystrom@gmail.com>
2020-12-25 23:05:59 +0100

    feat: Extend room visuals with rampart information

--- src/managers/roomManager/visualHandler.ts
+++ src/managers/roomManager/visualHandler.ts
@@ -1,4 +1,6 @@
+import { RoomManager } from "managers/roomManager";
 import { RunEvery } from "utils/RunEvery";
+import * as C from "../../config/constants";
 import * as Config from "../../config/config";
 import { getFromCache, saveToCache } from "../../utils/Cache";
 
@@ -6,7 +8,10 @@ export function VisualHandler(room: Room): void {
     if (Config.roomVisuals && room.controller !== undefined && room.controller.my && room.memory.roomLevel === 2) {
         let data: string | null = getFromCache("visualhandlerdata" + room.name, 10);
         if (data === null) {
-            room.visual.text("rt:" + Object.values(room.memory.repairTargets).length, 1, 1);
+            room.visual.text("Repair Targets: " + Object.values(room.memory.repairTargets).length, 1, 1.275, {
+                align: "left",
+                font: "0.8"
+            });
             for (const site of Object.values(room.memory.repairTargets)) {
                 if (site.roomName === room.name) {
                     room.visual.circle(site.x, site.y, {
@@ -14,6 +19,51 @@ export function VisualHandler(room: Room): void {
                     });
                 }
             }
+            const ramparts = room.find(FIND_MY_STRUCTURES, { filter: (s) => s.structureType === STRUCTURE_RAMPART });
+
+            let sum = 0;
+
+            let hitsMax = 0;
+            let hitsMin = Infinity;
+
+            for (const rampart of ramparts) {
+                if (hitsMax < rampart.hits) {
+                    hitsMax = rampart.hits;
+                }
+                if (hitsMin > rampart.hits) {
+                    hitsMin = rampart.hits;
+                }
+                sum += rampart.hits;
+            }
+
+            room.visual.text(
+                "Rampart Health Target: " +
+                    Math.round((RAMPART_HITS_MAX[room.controller.level] * C.RAMPART_PERCENTAGE_MIN) / 1000) +
+                    "k",
+                1,
+                2.275,
+                {
+                    align: "left",
+                    font: "0.8"
+                }
+            );
+
+            room.visual.text(
+                "Rampart Health: " +
+                    Math.round(sum / ramparts.length / 1000) +
+                    "k/" +
+                    Math.round(hitsMin / 1000) +
+                    "k/" +
+                    Math.round(hitsMax / 1000) +
+                    "k (avg/min/max)",
+                1,
+                3.275,
+                {
+                    align: "left",
+                    font: "0.8"
+                }
+            );
+
             saveToCache("visualhandlerdata" + room.name, room.visual.export());
         } else {
             room.visual.import(data);
