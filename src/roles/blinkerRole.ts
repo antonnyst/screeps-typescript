cd9fadba121eaac9f264a1ff4357b6ea0d80913e
demonitaste <nan.nystrom@gmail.com>
2021-05-07 18:04:05 +0200

    feat: Improve blinker logic

--- src/roles/blinkerRole.ts
+++ src/roles/blinkerRole.ts
@@ -17,9 +17,18 @@ export class BlinkerRole extends CreepRole {
         if (this.creep.room.name !== this.creep.memory.roleData.target) {
             this.setMovementData(new RoomPosition(25, 25, this.creep.memory.roleData.target), 20, false, false);
         } else {
-            const target: Structure | Creep | null = Game.getObjectById(this.creep.memory.roleData.targetId as string);
+            const target: Structure | Creep | ConstructionSite | null = Game.getObjectById(
+                this.creep.memory.roleData.targetId as string
+            );
             if (target != null) {
-                this.setMovementData(target.pos, 3, false, false);
+                if (target instanceof ConstructionSite) {
+                    this.setMovementData(target.pos, 0, false, false);
+                    if (target.pos.getRangeTo(this.creep.pos.x, this.creep.pos.y) === 0) {
+                        this.setMovementData(target.pos, 1, true, false);
+                    }
+                } else {
+                    this.setMovementData(target.pos, 3, false, false);
+                }
 
                 const r = this.creep.pos.getRangeTo(target.pos);
                 if (r <= 3) {
@@ -27,9 +36,10 @@ export class BlinkerRole extends CreepRole {
                     if (t != null && this.creep.pos.getRangeTo(t.pos) <= 3) {
                         this.creep.rangedAttack(t);
                     } else {
-                        this.creep.rangedAttack(target);
+                        if (!(target instanceof ConstructionSite)) {
+                            this.creep.rangedAttack(target);
+                        }
                     }
-                    //
                 } else {
                     const t = this.creep.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
                     if (t != null) {
@@ -37,14 +47,25 @@ export class BlinkerRole extends CreepRole {
                     }
                 }
             } else {
-                const t = this.creep.pos.findClosestByPath(FIND_HOSTILE_STRUCTURES, {
+                const t = this.creep.pos.findClosestByRange(FIND_HOSTILE_STRUCTURES, {
                     filter: (s) =>
-                        s.structureType !== STRUCTURE_STORAGE ||
-                        (s.structureType === STRUCTURE_STORAGE && s.store.getUsedCapacity() === 0)
+                        (s.structureType !== STRUCTURE_STORAGE ||
+                            (s.structureType === STRUCTURE_STORAGE && s.store.getUsedCapacity() === 0)) &&
+                        s.hits !== undefined
                 });
 
                 if (t != null) {
                     this.creep.memory.roleData.targetId = t.id;
+                } else {
+                    const hc = this.creep.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
+                    if (hc !== null) {
+                        this.creep.memory.roleData.targetId = hc.id;
+                    } else {
+                        const cs = this.creep.pos.findClosestByRange(FIND_HOSTILE_CONSTRUCTION_SITES);
+                        if (cs !== null) {
+                            this.creep.memory.roleData.targetId = cs.id;
+                        }
+                    }
                 }
             }
         }
