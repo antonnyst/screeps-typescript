f25370f53846d114220d845327bd2310e0cb62a5
demonitaste <nan.nystrom@gmail.com>
2021-08-07 09:39:16 +0200

    Improve quickFiller logic

--- src/roles/quickFillerRole.ts
+++ src/roles/quickFillerRole.ts
@@ -14,46 +14,58 @@ export class QuickFiller extends CreepRole {
             return;
         }
 
-        const targets: (StructureExtension | StructureSpawn)[] = GetEnergyBuildings(
-            Memory.rooms[this.creep.memory.home].genBuildings!.spawns,
-            this.creep.pos
-        ).concat(GetEnergyBuildings(Memory.rooms[this.creep.memory.home].genBuildings!.extensions, this.creep.pos)) as (
-            | StructureExtension
-            | StructureSpawn
-        )[];
-
         if (this.creep.memory.targetPos !== undefined) {
             this.setMovementData(unpackPosition(this.creep.memory.targetPos), 0, false, true);
             if (unpackPosition(this.creep.memory.targetPos).isEqualTo(this.creep.pos)) {
                 this.creep.memory.targetPos = undefined;
             }
-        }
+        } else {
+            const targets: (StructureExtension | StructureSpawn)[] = GetEnergyBuildings(
+                Memory.rooms[this.creep.memory.home].genBuildings!.spawns,
+                this.creep.pos
+            ).concat(
+                GetEnergyBuildings(Memory.rooms[this.creep.memory.home].genBuildings!.extensions, this.creep.pos)
+            ) as (StructureExtension | StructureSpawn)[];
 
-        if (targets.length > 0 && this.creep.memory.targetPos === undefined) {
-            if (this.creep.store.getUsedCapacity(RESOURCE_ENERGY) > 0) {
-                this.creep.transfer(targets[0], RESOURCE_ENERGY);
-            } else {
-                if (Memory.rooms[this.creep.memory.home].genBuildings!.links[2].id !== undefined) {
-                    const link = Game.getObjectById(Memory.rooms[this.creep.memory.home].genBuildings!.links[2].id!);
-                    if (link !== null && link instanceof StructureLink) {
-                        if (
-                            link.store.getUsedCapacity(RESOURCE_ENERGY) >=
-                            targets[0].store.getFreeCapacity(RESOURCE_ENERGY)
-                        ) {
-                            this.creep.withdraw(link, RESOURCE_ENERGY);
+            if (targets.length > 0) {
+                if (this.creep.store.getUsedCapacity(RESOURCE_ENERGY) > 0) {
+                    this.creep.transfer(targets[0], RESOURCE_ENERGY);
+                } else {
+                    if (Memory.rooms[this.creep.memory.home].genBuildings!.links[2].id !== undefined) {
+                        const link = Game.getObjectById(
+                            Memory.rooms[this.creep.memory.home].genBuildings!.links[2].id!
+                        );
+                        if (link !== null && link instanceof StructureLink) {
+                            if (
+                                link.store.getUsedCapacity(RESOURCE_ENERGY) >=
+                                targets[0].store.getFreeCapacity(RESOURCE_ENERGY)
+                            ) {
+                                this.creep.withdraw(link, RESOURCE_ENERGY);
+                            }
                         }
                     }
                 }
-            }
-        } else if (
-            this.creep.ticksToLive !== undefined &&
-            this.creep.ticksToLive < 10 &&
-            this.creep.store.getUsedCapacity(RESOURCE_ENERGY) > 0
-        ) {
-            const link = Game.getObjectById(Memory.rooms[this.creep.memory.home].genBuildings!.links[2].id!);
-            if (link !== null && link instanceof StructureLink) {
-                if (link.store.getFreeCapacity(RESOURCE_ENERGY) >= this.creep.store.getUsedCapacity(RESOURCE_ENERGY)) {
-                    this.creep.transfer(link, RESOURCE_ENERGY);
+            } else if (
+                this.creep.ticksToLive !== undefined &&
+                this.creep.ticksToLive < 10 &&
+                this.creep.store.getUsedCapacity(RESOURCE_ENERGY) > 0
+            ) {
+                const link = Game.getObjectById(Memory.rooms[this.creep.memory.home].genBuildings!.links[2].id!);
+                if (link !== null && link instanceof StructureLink) {
+                    if (
+                        link.store.getFreeCapacity(RESOURCE_ENERGY) >= this.creep.store.getUsedCapacity(RESOURCE_ENERGY)
+                    ) {
+                        this.creep.transfer(link, RESOURCE_ENERGY);
+                    }
+                }
+            } else if (this.creep.store.getFreeCapacity(RESOURCE_ENERGY) > 0) {
+                const link = Game.getObjectById(Memory.rooms[this.creep.memory.home].genBuildings!.links[2].id!);
+                if (link !== null && link instanceof StructureLink) {
+                    if (
+                        link.store.getUsedCapacity(RESOURCE_ENERGY) >= this.creep.store.getFreeCapacity(RESOURCE_ENERGY)
+                    ) {
+                        this.creep.withdraw(link, RESOURCE_ENERGY);
+                    }
                 }
             }
         }
