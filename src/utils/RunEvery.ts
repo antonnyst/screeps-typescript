7846d832a9e150107b0334acac28ccd50e060b6b
demonitaste <nan.nystrom@gmail.com>
2021-08-07 11:54:07 +0200

    Fix code not running on low tick values

--- src/utils/RunEvery.ts
+++ src/utils/RunEvery.ts
@@ -2,7 +2,8 @@ const times: { [key in string]: number } = {};
 
 export function RunEvery(f: (...n: any) => any, name: string, time: number, ...args: any) {
     if (times[name] === undefined) {
-        times[name] = 0;
+        times[name] = Game.time;
+        f(...args);
     }
 
     if (times[name] <= Game.time - time) {
