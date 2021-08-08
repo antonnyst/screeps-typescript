402f89bc1e642256f39e70f3fc8342d6f6736abb
demonitaste <nan.nystrom@gmail.com>
2021-08-08 17:00:09 +0200

    Add RoomCoordinate functions

---
+++ src/utils/RoomCoordinate.ts
@@ -0,0 +1,31 @@
+interface RoomCoordinate {
+    x: number;
+    y: number;
+}
+
+export function toRoomCoordinate(room: string): RoomCoordinate | null {
+    const match = room.match(/^([WE])([0-9]+)([NS])([0-9]+)$/);
+    if (match !== null) {
+        let [, h, rx, v, ry] = match;
+        let x = Number.parseInt(rx, 10);
+        let y = Number.parseInt(ry, 10);
+        if (h == "W") {
+            x = ~x;
+        }
+        if (v == "N") {
+            y = ~y;
+        }
+        return {
+            x,
+            y
+        };
+    }
+    return null;
+}
+
+export function fromRoomCoordinate(coordinate: RoomCoordinate): string {
+    return (
+        (coordinate.x < 0 ? "W" + ~coordinate.x : "E" + coordinate.x) +
+        (coordinate.y < 0 ? "N" + ~coordinate.y : "S" + coordinate.y)
+    );
+}
