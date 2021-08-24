3581acf445a990c2d450a38932e101570add67c9
demonitaste <nan.nystrom@gmail.com>
2021-08-24 20:15:19 +0200

    Make reserver attack hostile owned rooms

--- src/creeps/roles/reserver.ts
+++ src/creeps/roles/reserver.ts
@@ -1,5 +1,6 @@
 import { setMovementData } from "creeps/creep";
 import { unpackPosition } from "utils/RoomPositionPacker";
+import { PLAYER_USERNAME } from "utils/username";
 
 export interface ReserverMemory extends CreepMemory {
     room: string;
@@ -23,8 +24,10 @@ export function reserver(creep: Creep) {
             creep.signController(creep.room.controller, "");
         }
         if (
-            creep.room.controller.reservation !== undefined &&
-            creep.room.controller.reservation.username !== Game.spawns[Object.keys(Game.spawns)[0]].owner.username
+            (creep.room.controller.reservation !== undefined &&
+                creep.room.controller.reservation.username !==
+                    Game.spawns[Object.keys(Game.spawns)[0]].owner.username) ||
+            (creep.room.controller.owner !== undefined && creep.room.controller.owner.username !== PLAYER_USERNAME)
         ) {
             creep.attackController(creep.room.controller);
         } else {
