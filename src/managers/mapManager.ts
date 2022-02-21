bbb3cdc34ba072b7c2389a6671cf890abb6e73e6
antonnyst <nan.nystrom@gmail.com>
2022-02-21 20:54:32 +0100

    Improve map visuals

--- src/managers/mapManager.ts
+++ src/managers/mapManager.ts
@@ -27,23 +27,27 @@ export class MapManager implements Manager {
 
 const colors = ["#ff0000", "#ff0000", "#ffffff", "#00ff00", "#00ff00"];
 const opacity = [0.5, 0.25, 0, 0.25, 0.5];
-const size = 30;
+const size = [32, 16, 0, 16, 32];
 
 function perRoom(room: string) {
     const control = RoomData(room).control.get();
-    const x = (50 - size) / 2;
-    const y = (50 - size) / 2;
     if (control !== null) {
-        const tl: RoomPosition = new RoomPosition(x, y, room);
-
-        Game.map.visual.rect(tl, size, size, {
-            fill: colors[control + 2],
-            opacity: opacity[control + 2]
-        });
         const text: string = "A: " + (Game.time - (RoomData(room).lastUpdate.get() ?? 0));
         Game.map.visual.text(text, new RoomPosition(2, 4, room), {
             fontSize: 6,
             align: "left"
+        });
+
+        const s = size[control + 2];
+        if (s === 0) {
+            return;
+        }
+        const x = (50 - s) / 2;
+        const y = (50 - s) / 2;
+        const tl: RoomPosition = new RoomPosition(x, y, room);
+        Game.map.visual.rect(tl, s, s, {
+            fill: colors[control + 2],
+            opacity: opacity[control + 2]
         });
     }
 }
