66b5e0bc019c713df13322bcef5e9289a66850fe
antonnyst <nan.nystrom@gmail.com>
2022-06-19 18:18:53 +0200

    Add squad spawning logic

--- src/operation/runner.ts
+++ src/operation/runner.ts
@@ -1,9 +1,11 @@
 import * as OperationLogic from "./operations/";
+import { CreepDescriptor, Operation } from "./operation";
 import { GenerateBodyFromPattern, bodySortingValues } from "utils/CreepBodyGenerator";
-import { Operation } from "./operation";
 import { RunEvery } from "utils/RunEvery";
 import { generateName } from "utils/CreepNames";
 import { isOwnedRoom } from "utils/RoomCalc";
+import { runSquad } from "squad/runner";
+import { spawnSquad } from "squad/squad";
 
 export type OperationType = keyof typeof OperationLogic;
 
@@ -37,6 +39,9 @@ function runOperations(operations: Operation[], general: boolean): void {
       operations.splice(i, 1);
     } else {
       runOperations(operation.children, general);
+      for (const squad of operation.squads) {
+        runSquad(squad);
+      }
 
       if (general) {
         GeneralLogic(operation);
@@ -55,41 +60,76 @@ function GeneralLogic(operation: Operation): void {
     return;
   }
 
-  // Individual creep logic
+  // Creep spawning logic
   for (const creep of operation.creeps) {
-    // Spawn creeps with no current name
-    if (creep.current === undefined) {
-      const name = generateName();
-      const memory = creep.memory;
-      sourceBase.memory.spawnQueue.push({
-        memory,
-        body: GenerateBodyFromPattern(creep.body, sourceBase.energyCapacityAvailable).sort(
-          (a, b) => bodySortingValues[a] - bodySortingValues[b]
-        ),
-        name
-      });
+    spawnCreep(creep, sourceBase);
+  }
 
-      creep.current = name;
-      continue;
-    }
+  // Squad spawning logic
+  for (const squad of operation.squads) {
+    if (!squad.spawned) {
+      spawnSquad(squad, sourceBase);
+      squad.spawned = true;
+    } else {
+      let allDead = true;
+      for (const creep of squad.creeps) {
+        if (creep.current !== undefined && Game.creeps[creep.current] === undefined) {
+          let found = false;
+          for (const queuedCreep of sourceBase.memory.spawnQueue) {
+            if (queuedCreep.name === creep.current) {
+              found = true;
+              break;
+            }
+          }
 
-    // Handle creeps we cannot find
-    if (Game.creeps[creep.current] === undefined) {
-      // We cant find our current creep
-      // Check for it in spawn queues before removing current name
-
-      let found = false;
-      for (const queuedCreep of sourceBase.memory.spawnQueue) {
-        if (queuedCreep.name === creep.current) {
-          found = true;
-          break;
+          if (!found) {
+            creep.current = undefined;
+          }
+        }
+        if (creep.current !== undefined) {
+          allDead = false;
         }
       }
-
-      if (!found) {
-        creep.current = undefined;
-        continue;
+      if (allDead) {
+        squad.spawned = false;
       }
+    }
+  }
+}
+
+function spawnCreep(creep: CreepDescriptor, sourceBase: OwnedRoom): void {
+  // Spawn creeps with no current name
+  if (creep.current === undefined) {
+    const name = generateName();
+    const memory = creep.memory;
+    sourceBase.memory.spawnQueue.push({
+      memory,
+      body: GenerateBodyFromPattern(creep.body, sourceBase.energyCapacityAvailable).sort(
+        (a, b) => bodySortingValues[a] - bodySortingValues[b]
+      ),
+      name
+    });
+
+    creep.current = name;
+    return;
+  }
+
+  // Handle creeps we cannot find
+  if (Game.creeps[creep.current] === undefined) {
+    // We cant find our current creep
+    // Check for it in spawn queues before removing current name
+
+    let found = false;
+    for (const queuedCreep of sourceBase.memory.spawnQueue) {
+      if (queuedCreep.name === creep.current) {
+        found = true;
+        break;
+      }
+    }
+
+    if (!found) {
+      creep.current = undefined;
+      return;
     }
   }
 }
--- src/squad/squad.ts
+++ src/squad/squad.ts
@@ -1,9 +1,28 @@
+import { GenerateBodyFromPattern, bodySortingValues } from "utils/CreepBodyGenerator";
 import { CreepDescriptor } from "operation/operation";
 import { SquadType } from "./runner";
+import { generateName } from "utils/CreepNames";
 
 // Contains information about a squad
 export interface Squad {
   type: SquadType;
   creeps: CreepDescriptor[];
   spawned?: boolean;
+}
+
+export function spawnSquad(squad: Squad, sourceBase: OwnedRoom): void {
+  // Spawn creeps
+  for (const creep of squad.creeps) {
+    const name = generateName();
+    const memory = creep.memory;
+    sourceBase.memory.spawnQueue.push({
+      memory,
+      body: GenerateBodyFromPattern(creep.body, sourceBase.energyCapacityAvailable).sort(
+        (a, b) => bodySortingValues[a] - bodySortingValues[b]
+      ),
+      name
+    });
+
+    creep.current = name;
+  }
 }
